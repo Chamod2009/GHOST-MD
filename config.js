a950ba13c59a332d9fdf757cba7622601d0c6e52
Chamod2009 <cmax3226@gmail.com>
2024-09-03 17:02:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID,cdxVDQ6C#4GMbDufuHTQKB203KNz24vB5eBmvNM2LYj5H0arYN1M
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "Chamod - md",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
